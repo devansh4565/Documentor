512a28176a384ba3bba6821d7960a054cd3c8340
devansh4565 <devansh4565@gmail.com>
2025-07-05 20:21:37 +0530

    `Update import statements in UserContext and ThemeContext components to include useMemo and useCallback`

--- frontend/src/context/ThemeContext.jsx
+++ frontend/src/context/ThemeContext.jsx
@@ -1,5 +1,5 @@
 // src/context/ThemeContext.jsx
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState, useMemo, useCallback } from "react";
 
 export const ThemeContext = createContext();
 
